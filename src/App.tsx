e0a60e2ad90ec9c059fd25d4d14dc1e69fbf73e8
AustynYou <austyn.you@gmail.com>
2023-04-13 18:42:06 -0700

    ✨ Implement to show Alert after success

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Contact from "./components/Contact";
+
+import Alert from "@mui/material/Alert";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
 function App() {
+  const [alert, setAlert] = React.useState<boolean>(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ "& > :not(style)": { m: 1 } }}>
+      {alert && (
+        <Stack spacing={2}>
+          <Alert onClose={()=>setAlert(false)} severity="success">
+            message sent!
+          </Alert>
+        </Stack>
+      )}
+      <Typography variant="h4" component="h1" gutterBottom>
+        Hey, keep in touch with austyn!
+      </Typography>
+      <Contact setAlert={setAlert} />
+    </Box>
   );
 }
 
